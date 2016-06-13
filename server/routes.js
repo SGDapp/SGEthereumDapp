531eb6522dc67697bf22fc55f9cebcf0c33faa5b
Sanjeev Hegde <sanjeevh.93@gmail.com>
2016-06-14 00:04:44 +0530

    modified payements , added escrow rest methods

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,8 @@ var fs = require('fs');
 var hashFiles = require('hash-files');
 var multer  = require('multer');
 var Web3= require('web3');
-
+var baseUrl = "/ethereumDapp/";
+ 
 
 var web3NodeA = new Web3();
 web3NodeA.setProvider(new web3NodeA.providers.HttpProvider('http://localhost:8000')); 
@@ -19,18 +20,24 @@ web3NodeB.personal.unlockAccount(web3NodeB.eth.accounts[2],'1234',50000);
 //console.log(web3NodeA.isConnected());
 //console.log(web3NodeB.isConnected());
 
+var customTransactionLog = function(message,txId,transaction){
 
+	console.log("---- "+ message +" ----");
+	console.log("Transactionhash: "+ txId);
+	console.log("Transaction Details: ");
+	console.log(transaction);
+	console.log("-----------------------------");
+}
 
 var appRouter = function(app) {
-var wallet = require('./features/payements.js')(app,web3NodeA,web3NodeB);
-//var escrow = require('./features/escrow.js')(app,web3NodeB);
-//var digitizingDocs = require('./features/digitizingDocs.js')(app,web3NodeB);
-//var letterOfCredit = require('./features/letterOfCreditjs')(app,web3NodeB);
+	require('./features/payements.js')(app,web3NodeA,web3NodeB,baseUrl,customTransactionLog);
+	require('./features/escrow.js')(app,web3NodeB,baseUrl,customTransactionLog);
+	//require('./features/digitizingDocs.js')(app,web3NodeB);
+ 	//require('./features/letterOfCreditjs')(app,web3NodeB);
 
 
 app.get("/", function(req, res) {
-	//console.log(web3NodeA.isConnected());
-	//console.log(web3NodeB.isConnected());
+	
    	 res.send("Hello World");
 });
 
