2e62732fd1ed9845c2aa064b9d8006c11f079cd3
Sanjeev Hegde <sanjeevh.93@gmail.com>
2016-06-24 00:41:30 +0530

    added contract details display in Buyers page

--- etherumDapp/dest/main.js
+++ etherumDapp/dest/main.js
@@ -459,7 +459,7 @@ event.watch(function(error, result){
 },{}],6:[function(require,module,exports){
 var app = angular.module('dapp.loc.buyer', ['ui.router']);
 app.controller('buyerCtrl', function($scope,Api) {
-
+$scope.lstatus=7;
 	$scope.change= function(){
 		$scope.lstatus=$scope.no;
 
--- etherumDapp/src/loc/buyer/buyer.js
+++ etherumDapp/src/loc/buyer/buyer.js
@@ -1,6 +1,6 @@
 var app = angular.module('dapp.loc.buyer', ['ui.router']);
 app.controller('buyerCtrl', function($scope,Api) {
-
+$scope.lstatus=7;
 	$scope.change= function(){
 		$scope.lstatus=$scope.no;
 
