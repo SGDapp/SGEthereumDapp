aff85e512138465c736496a00fbbafa12205edd1
Akshay <akshaynaik033@gmail.com>
2016-06-26 04:02:40 +0530

    seller page created

--- etherumDapp/src/loc/seller/seller.js
+++ etherumDapp/src/loc/seller/seller.js
@@ -1,5 +1,20 @@
 var app = angular.module('dapp.loc.seller', ['ui.router']);
 app.controller('sellerCtrl', function($scope,Api) {
+$scope.lcStatus=2;
+if($scope.lcStatus<=2){
+    $scope.message="Waiting for LC to be issued...";
+}
 
 
+if($scope.lcStatus>=4 && $scope.lcStatus>=10){
+    $scope.message="Goods Dispatched. Waiting for Payment to be made..";
+}
+
+if($scope.lcStatus==10){
+    $scope.message="Amount Received..";
+}
+
+$scope.dispatchGoods =function(){
+		$scope.digitallySign=true;
+	}
 });
