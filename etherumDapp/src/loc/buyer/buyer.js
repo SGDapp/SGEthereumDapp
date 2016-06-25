2c03fbfb7ebb91192920ba01476d1fd3a83bb2d5
sanjeev-hegde <sanjeevh.93@gmail.com>
2016-06-26 03:57:06 +0530

    modified:   etherumDapp/src/loc/buyer/buyer.js

--- etherumDapp/src/loc/buyer/buyer.js
+++ etherumDapp/src/loc/buyer/buyer.js
@@ -1,11 +1,14 @@
 var app = angular.module('dapp.loc.buyer', ['ui.router']);
 app.controller('buyerCtrl', function($scope,Api) {
-$scope.lcStatus=8;
+$scope.lcStatus=10;
 if($scope.lcStatus>0 && $scope.lcStatus<7){
 	$scope.waitingMessage="Waiting For Goods...";
 }
 else if ($scope.lcStatus>7 && $scope.lcStatus<9){
 	$scope.waitingMessage="Waiting For Banks Confirmation To Make Payement...";
+}
+else{
+	$scope.waitingMessage="Payement Done";
 }
 $scope.goodsReceived=false;
 	$scope.change= function(){
