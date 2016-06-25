dbe370d8cd297dc3a3bc63a469a5cc2b350ab54d
sanjeev-hegde <sanjeevh.93@gmail.com>
2016-06-26 02:06:49 +0530

    updated buyerUI 	modified:   etherumDapp/src/loc/buyer/buyer.js

--- etherumDapp/src/loc/buyer/buyer.js
+++ etherumDapp/src/loc/buyer/buyer.js
@@ -1,9 +1,19 @@
 var app = angular.module('dapp.loc.buyer', ['ui.router']);
 app.controller('buyerCtrl', function($scope,Api) {
-$scope.lstatus=3;
+$scope.lcStatus=0;
+$scope.goodsReceived=false;
 	$scope.change= function(){
 		$scope.lstatus=$scope.no;
 
+	}
+	$scope.requestLC =function(){
+		$scope.digitallySign=true;
+	}
+	$scope.goodsReceived =function(){
+		$scope.digitallySign=true;
+	}
+	$scope.makePayment =function(){
+		$scope.digitallySign=true;
 	}
 
 });
