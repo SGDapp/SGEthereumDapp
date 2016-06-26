9e090c882471b6021340859350d89d6145448597
sanjeev-hegde <sanjeevh.93@gmail.com>
2016-06-26 18:05:46 +0530

    letter of  credit pdf creation and upload methods

--- etherumDapp/src/loc/ibank/issuingBank.js
+++ etherumDapp/src/loc/ibank/issuingBank.js
@@ -1,5 +1,42 @@
 var app = angular.module('dapp.loc.ibank', ['ui.router']);
-app.controller('issuingBankCtrl', function($scope,Api) {
+var customPdfGenerator = require('./pdfContents.js');
+app.controller('issuingBankCtrl', function($scope,Api,$http) {
 
+    var lcPdf = customPdfGenerator.createPdf();
+
+  // lcPdf.output('save', 'LetterOfCredit.pdf'); //Try to save PDF as a file (not works on ie before 10, and some mobile devices)
+  /*
+      var pdfData = lcPdf.output('arraybuffer')
+      alternative way to save as blob
+              var pdf = new Blob([pdfData], {
+                  type: 'application/pdf'
+              });
+
+  */
+      var pdf = lcPdf.output('blob');
+    /*
+    // open pdf in new window
+      var fileURL = URL.createObjectURL(pdf);
+      window.open(fileURL);
+*/
+      var data = new FormData();
+      data.append('pdfFile' , pdf);
+
+                var request = {
+                    method: 'POST',
+                    url: '/ethereumDapp/uploadLC',
+                    data: data,
+                    headers: {
+                        'Content-Type': undefined
+                    }
+                };
+
+                // SEND THE FILES.
+                $http(request)
+                    .success(function (d) {
+                      //console.log(d);
+                    })
+                    .error(function () {
+                    });
 
 });
--- server/routes.js
+++ server/routes.js
@@ -3,10 +3,12 @@ var hashFiles = require('hash-files');
 var multer  = require('multer');
 var Web3= require('web3');
 var baseUrl = "/ethereumDapp/";
- 
+
+var upload = multer({dest:'./uploads/'});
+var pdfUpload = upload.single('pdfFile');
 
 var web3NodeA = new Web3();
-web3NodeA.setProvider(new web3NodeA.providers.HttpProvider('http://localhost:8000')); 
+web3NodeA.setProvider(new web3NodeA.providers.HttpProvider('http://localhost:8000'));
 var web3NodeB = new Web3();
 web3NodeB.setProvider(new web3NodeB.providers.HttpProvider('http://localhost:8001'));
 web3NodeA.personal.unlockAccount(web3NodeA.eth.accounts[0],'1234',50000);
@@ -15,7 +17,7 @@ web3NodeA.personal.unlockAccount(web3NodeA.eth.accounts[2],'1234',50000);
 web3NodeB.personal.unlockAccount(web3NodeB.eth.accounts[0],'1234',50000);
 web3NodeB.personal.unlockAccount(web3NodeB.eth.accounts[1],'1234',50000);
 web3NodeB.personal.unlockAccount(web3NodeB.eth.accounts[2],'1234',50000);
- 
+
 
 
 
@@ -35,11 +37,25 @@ var appRouter = function(app) {
  	//require('./features/letterOfCreditjs')(app,web3NodeB);
 
 
-app.get("/", function(req, res) {
-	
+app.get("/ethereumDapp/", function(req, res) {
+
    	 res.send("Hello World");
 });
+app.post("/ethereumDapp/uploadLC", pdfUpload, function(req, res) {
 
+ //console.log(req);
+ console.log(req.file.path);
+ var tmp_path = req.file.path;
+ var target_path="./targetFiles/LetterOfCredit/LetterOfCredit.pdf";
+ fs.rename(tmp_path, target_path, function(err) {
+       if (err) throw err;
+       // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+         fs.unlink(tmp_path, function() {
+             if (err) throw err;
+         });
+  });
+res.status(200).end();
+});
 
 }
 module.exports = appRouter;
