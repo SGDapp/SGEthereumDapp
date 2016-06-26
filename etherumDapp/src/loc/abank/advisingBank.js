1eef648897be6600ace73c65697ace630b5287db
Akshay <akshaynaik033@gmail.com>
2016-06-26 17:27:06 +0530

    Advising bank and Issuing Bank

--- etherumDapp/src/loc/abank/advisingBank.js
+++ etherumDapp/src/loc/abank/advisingBank.js
@@ -1,5 +1,16 @@
 var app = angular.module('dapp.loc.abank', ['ui.router']);
 app.controller('advisingBankCtrl', function($scope,Api) {
-$scope.msg="abank";
+$scope.lcStatus=10
+
+$scope.verifyLC= function(){
+    $scope.digitallySign=true;
+}
+
+if($scope.lcStatus>1 && $scope.lcStatus<=9){
+$scope.message="LC Verified.Waiting for payment to be made..."
+}
+else if($scope.lcStatus==10){
+$scope.message="Payment Made."
+}
 
 });
--- etherumDapp/src/loc/ibank/issuingBank.js
+++ etherumDapp/src/loc/ibank/issuingBank.js
@@ -1,5 +1,29 @@
 var app = angular.module('dapp.loc.ibank', ['ui.router']);
 app.controller('issuingBankCtrl', function($scope,Api) {
+$scope.lcStatus=5;
+$scope.message="";
 
+console.log($scope.issuingBankName);
+
+$scope.createLC = function(){
+    $scope.isLcCreated= true;
+}
+
+
+$scope.issueLC = function () {
+    $scope.digitallySign=true;
+} 
+
+$scope.makePaymentPermission =function(){
+    $scope.digitallySign=true;
+}
+
+if($scope.lcStatus>1 && $scope.lcStatus<9){
+$scope.message="LC issued . Waiting for buyer confirmation on receiving goods..."
+}
+
+if($scope.lcStatus>9){
+$scope.message="Payment Done"
+}
 
 });
