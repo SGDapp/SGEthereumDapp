3ee873ef3efa32fe31dfc09217006f17b8dbe4d2
Akshay <akshaynaik033@gmail.com>
2016-06-28 00:19:56 +0530

    Shipping agent.

--- etherumDapp/src/loc/sagent/shippingAgent.js
+++ etherumDapp/src/loc/sagent/shippingAgent.js
@@ -1,5 +1,22 @@
 var app = angular.module('dapp.loc.sagent', ['ui.router']);
 app.controller('shippingAgentCtrl', function($scope,Api) {
+$scope.message=""
+$scope.lcStatus=4;
+$scope.goodsDispatched = function(){
+    $scope.digitallySign=true;
+}
 
+$scope.uploadBol = function(){
+    $scope.digitallySign=true;
+}
+
+
+$scope.goodsDelivered = function(){
+    $scope.digitallySign=true;
+}
+
+if($scope.lcStatus>6){
+    $scope.message="Goods Delivered."
+}
 
 });
